77bf780e08c700371bc8bb90430d28637b68517b
ibtashamalee <aleejaan562@gmail.com>
2020-08-05 02:38:34 +0500

    icon update

--- src/views/SubjectDetail.vue
+++ src/views/SubjectDetail.vue
@@ -3,27 +3,34 @@
     <header class="bg-white shadow max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-center sm:items-stretch sm:justify-start h-20">
         <div class="flex-shrink-0 flex items-center">
-          <div class="h-10 w-10 mr-3 bg-primary-light rounded-full flex items-center justify-center">
-            <svg class="text-on-primary h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
+          <div
+            class="h-10 w-10 mr-3 bg-primary-light rounded-full flex items-center justify-center">
+            <svg class="text-on-primary h-5 w-5" fill="none" stroke="currentColor"
+                 stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+              <path
+                d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
             </svg>
           </div>
         </div>
         <div class="min-w-0 flex-1 flex flex-col justify-center">
-          <div class="text-sm leading-5 font-medium text-accent truncate">Data Structures & algorithm</div>
-          <div class="mt-1 text-sm leading-5 text-primary-light truncate">CSC231 - Dr. Farukh Zeeshan</div>
+          <div class="text-sm leading-5 font-medium text-accent truncate">Data Structures &
+            algorithm
+          </div>
+          <div class="mt-1 text-sm leading-5 text-primary-light truncate">CSC231 - Dr. Farukh
+            Zeeshan
+          </div>
         </div>
-<!--        <div class="hidden sm:ml-6 sm:flex">-->
-<!--          <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-accent text-sm font-medium leading-5 text-primary-dark focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">-->
-<!--            Resources-->
-<!--          </a>-->
-<!--          <a href="#" class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-primary-light hover:text-primary-dark hover:border-gray-300 focus:outline-none focus:text-primary-dark focus:border-gray-300 transition duration-150 ease-in-out">-->
-<!--            Tasks-->
-<!--          </a>-->
-<!--          <a href="#" class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-primary-light hover:text-primary-dark hover:border-gray-300 focus:outline-none focus:text-primary-dark focus:border-gray-300 transition duration-150 ease-in-out">-->
-<!--            About-->
-<!--          </a>-->
-<!--        </div>-->
+        <!--        <div class="hidden sm:ml-6 sm:flex">-->
+        <!--          <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-accent text-sm font-medium leading-5 text-primary-dark focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">-->
+        <!--            Resources-->
+        <!--          </a>-->
+        <!--          <a href="#" class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-primary-light hover:text-primary-dark hover:border-gray-300 focus:outline-none focus:text-primary-dark focus:border-gray-300 transition duration-150 ease-in-out">-->
+        <!--            Tasks-->
+        <!--          </a>-->
+        <!--          <a href="#" class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-primary-light hover:text-primary-dark hover:border-gray-300 focus:outline-none focus:text-primary-dark focus:border-gray-300 transition duration-150 ease-in-out">-->
+        <!--            About-->
+        <!--          </a>-->
+        <!--        </div>-->
       </div>
     </header>
 
@@ -35,19 +42,21 @@
           <li class="border-t border-border-light">
             <div class="flex items-center px-4 py-4 sm:px-6">
               <div class="flex-shrink-0">
-                <div class="h-10 w-10 mr-3 bg-primary-light rounded-full flex items-center justify-center">
-                  <svg class="text-on-primary h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
-                  </svg>
+                <div class="h-10 w-10 mr-4 rounded-full flex items-center justify-center">
+                  <img class=" w-8" src="../assets/doc.svg">
                 </div>
               </div>
               <div class="min-w-0 flex-1 flex flex-col">
-                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 1 Slides</div>
-                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd, 2020</div>
+                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 1 Slides
+                </div>
+                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd,
+                  2020
+                </div>
               </div>
               <div class="flex">
-                <a href="#" class="text-primary-light hover:text-primary-dark">
-                  <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <a class="text-primary-light hover:text-primary-dark" href="#">
+                  <svg class="h-6 w-6" fill="none" stroke="currentColor" stroke-linecap="round"
+                       stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
                     <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                   </svg>
                 </a>
@@ -57,19 +66,21 @@
           <li class="border-t border-border-light">
             <div class="flex items-center px-4 py-4 sm:px-6">
               <div class="flex-shrink-0">
-                <div class="h-10 w-10 mr-3 bg-primary-light rounded-full flex items-center justify-center">
-                  <svg class="text-on-primary h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
-                  </svg>
+                <div class="h-10 w-10 mr-3 rounded-full flex items-center justify-center">
+                  <img class="w-8" src="../assets/doc.svg">
                 </div>
               </div>
               <div class="min-w-0 flex-1 flex flex-col">
-                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 2 Slides</div>
-                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd, 2020</div>
+                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 2 Slides
+                </div>
+                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd,
+                  2020
+                </div>
               </div>
               <div class="flex">
-                <a href="#" class="text-primary-light hover:text-primary-dark">
-                  <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <a class="text-primary-light hover:text-primary-dark" href="#">
+                  <svg class="h-6 w-6" fill="none" stroke="currentColor" stroke-linecap="round"
+                       stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
                     <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                   </svg>
                 </a>
@@ -79,19 +90,21 @@
           <li class="border-t border-border-light">
             <div class="flex items-center px-4 py-4 sm:px-6">
               <div class="flex-shrink-0">
-                <div class="h-10 w-10 mr-3 bg-primary-light rounded-full flex items-center justify-center">
-                  <svg class="text-on-primary h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
-                  </svg>
+                <div class="h-10 w-10 mr-4 rounded-full flex items-center justify-center">
+                  <img class=" w-8" src="../assets/xls.svg">
                 </div>
               </div>
               <div class="min-w-0 flex-1 flex flex-col">
-                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 3 Slides</div>
-                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd, 2020</div>
+                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 3 Slides
+                </div>
+                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd,
+                  2020
+                </div>
               </div>
               <div class="flex">
-                <a href="#" class="text-primary-light hover:text-primary-dark">
-                  <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <a class="text-primary-light hover:text-primary-dark" href="#">
+                  <svg class="h-6 w-6" fill="none" stroke="currentColor" stroke-linecap="round"
+                       stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
                     <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                   </svg>
                 </a>
@@ -101,19 +114,21 @@
           <li class="border-t border-border-light">
             <div class="flex items-center px-4 py-4 sm:px-6">
               <div class="flex-shrink-0">
-                <div class="h-10 w-10 mr-3 bg-primary-light rounded-full flex items-center justify-center">
-                  <svg class="text-on-primary h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
-                  </svg>
+                <div class="h-10 w-10 mr-4 rounded-full flex items-center justify-center">
+                  <img class=" w-8" src="../assets/ppt.svg">
                 </div>
               </div>
               <div class="min-w-0 flex-1 flex flex-col">
-                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 4 Slides</div>
-                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd, 2020</div>
+                <div class="text-sm leading-5 font-medium text-accent truncate">Lecture 4 Slides
+                </div>
+                <div class="mt-1 text-sm leading-5 text-primary-light truncate">February 23rd,
+                  2020
+                </div>
               </div>
               <div class="flex">
-                <a href="#" class="text-primary-light hover:text-primary-dark">
-                  <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <a class="text-primary-light hover:text-primary-dark" href="#">
+                  <svg class="h-6 w-6" fill="none" stroke="currentColor" stroke-linecap="round"
+                       stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
                     <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                   </svg>
                 </a>
