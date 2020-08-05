134d189cca3a19905729b8d0c5007de3a7052d30
ibtashamalee <aleejaan562@gmail.com>
2020-08-06 01:10:14 +0500

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="sticky w-full top-0">
     <nav class="bg-dark shadow-sm">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center h-16">
